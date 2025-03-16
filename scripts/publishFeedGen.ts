c43281dd9ae844900a1a51c1b9b5bc44f36bbf2e
Lenzi Erickson <lenzi.erickson@gmail.com>
2025-03-16 09:50:26 -0700

    trying to publish

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -11,66 +11,79 @@ const run = async () => {
     throw new Error('Please provide a hostname in the .env file')
   }
 
-  const answers = await inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'handle',
-        message: 'Enter your Bluesky handle:',
-        required: true,
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Enter your Bluesky password (preferably an App Password):',
-      },
-      {
-        type: 'input',
-        name: 'service',
-        message: 'Optionally, enter a custom PDS service to sign in with:',
-        default: 'https://bsky.social',
-        required: false,
-      },
-      {
-        type: 'input',
-        name: 'recordName',
-        message: 'Enter a short name or the record. This will be shown in the feed\'s URL:',
-        required: true,
-      },
-      {
-        type: 'input',
-        name: 'displayName',
-        message: 'Enter a display name for your feed:',
-        required: true,
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Optionally, enter a brief description of your feed:',
-        required: false,
-      },
-      {
-        type: 'input',
-        name: 'avatar',
-        message: 'Optionally, enter a local path to an avatar that will be used for the feed:',
-        required: false,
-      },
-      {
-        type: 'confirm',
-        name: 'videoOnly',
-        message: 'Is this a video-only feed? If so, do you want to set the content mode to video? This will allow for an "immersive" video experience within the app.',
-        default: false,
-      }
-    ])
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'handle',
+      message: 'Enter your Bluesky handle:',
+      required: true,
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Enter your Bluesky password (preferably an App Password):',
+    },
+    {
+      type: 'input',
+      name: 'service',
+      message: 'Optionally, enter a custom PDS service to sign in with:',
+      default: 'https://bsky.social',
+      required: false,
+    },
+    {
+      type: 'input',
+      name: 'recordName',
+      message:
+        "Enter a short name or the record. This will be shown in the feed's URL:",
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'displayName',
+      message: 'Enter a display name for your feed:',
+      required: true,
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Optionally, enter a brief description of your feed:',
+      required: false,
+    },
+    {
+      type: 'input',
+      name: 'avatar',
+      message:
+        'Optionally, enter a local path to an avatar that will be used for the feed:',
+      required: false,
+    },
+    {
+      type: 'confirm',
+      name: 'videoOnly',
+      message:
+        'Is this a video-only feed? If so, do you want to set the content mode to video? This will allow for an "immersive" video experience within the app.',
+      default: false,
+    },
+  ])
 
-  const { handle, password, recordName, displayName, description, avatar, service, videoOnly } = answers
+  const {
+    handle,
+    password,
+    recordName,
+    displayName,
+    description,
+    avatar,
+    service,
+    videoOnly,
+  } = answers
 
   const feedGenDid =
     process.env.FEEDGEN_SERVICE_DID ?? `did:web:${process.env.FEEDGEN_HOSTNAME}`
 
   // only update this if in a test environment
-  const agent = new AtpAgent({ service: service ? service : 'https://bsky.social' })
-  await agent.login({ identifier: handle, password})
+  const agent = new AtpAgent({
+    service: service ? service : 'https://bsky.social',
+  })
+  await agent.login({ identifier: handle, password })
 
   let avatarRef: BlobRef | undefined
   if (avatar) {
@@ -99,7 +112,9 @@ const run = async () => {
       description: description,
       avatar: avatarRef,
       createdAt: new Date().toISOString(),
-      contentMode: videoOnly ? AppBskyFeedDefs.CONTENTMODEVIDEO : AppBskyFeedDefs.CONTENTMODEUNSPECIFIED,
+      contentMode: videoOnly
+        ? AppBskyFeedDefs.CONTENTMODEVIDEO
+        : AppBskyFeedDefs.CONTENTMODEUNSPECIFIED,
     },
   })
 
